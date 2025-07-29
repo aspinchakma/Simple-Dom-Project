91385bf714b03689621cf212a6f9ded8996323dc
Aspin Chakma <aspinchakma2019@gmail.com>
2025-07-30 01:54:30 +0600

    finished button coloring

--- scripts/home.js
+++ scripts/home.js
@@ -1,10 +1,15 @@
-import { hiddenFunc } from "./utilities.js";
+import { changingButtonColorFunc, hiddenFunc } from "./utilities.js";
 // Donation Button
 document.getElementById("donation_button").addEventListener("click", () => {
   // donation section visibale
   hiddenFunc("donation_section", false);
   // history section hidden
   hiddenFunc("history_section", true);
+
+  //changing color for donation button
+  changingButtonColorFunc("donation_button", true);
+  // changing color for history button
+  changingButtonColorFunc("history_button", false);
 });
 
 document.getElementById("history_button").addEventListener("click", () => {
@@ -12,4 +17,9 @@ document.getElementById("history_button").addEventListener("click", () => {
   hiddenFunc("history_section", false);
   // hide donation section
   hiddenFunc("donation_section", true);
+
+  //changing color for donation button
+  changingButtonColorFunc("donation_button", false);
+  // changing color for history button
+  changingButtonColorFunc("history_button", true);
 });
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -7,3 +7,15 @@ export function hiddenFunc(id, isHidden) {
     getId.classList.remove("hidden");
   }
 }
+
+// changing button color section
+export function changingButtonColorFunc(id, isGreen) {
+  const getId = document.getElementById(id);
+  if (isGreen) {
+    getId.style.backgroundColor = "#B4F461";
+    getId.style.border = "2px solid #B4F461";
+  } else {
+    getId.style.backgroundColor = "white";
+    getId.style.border = "2px solid #b1b1b1";
+  }
+}
